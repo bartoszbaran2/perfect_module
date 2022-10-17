8221e007def00cb91fcabf51b4089f56a4ec6dd3
Bartosz Baran <bartosz.baran192@gmail.com>
2022-10-17 21:45:56 +0200

    better version of app with DRY, re-usability, hashbang

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python
+# dobra praktyka
 
-with open('text.txt', 'r') as file:
-    for line in file.readlines():
-        for word in line.split():
-            print(word)
+import sys
 
 
+def fetch_words(filepath):
+    with open(filepath, 'r') as file:
+        return [word for line in file for word in line.split()]
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(filepath):
+    words = fetch_words(filepath)
+    print_items(words)
+
+
+# read_file('text.txt')
+# print(__name__)  # zeby odróżnić __
+
+if __name__ == '__main__':  # sprawdza, czy w globalnym contexcie, nie odpala przy imporcie tylko po kropce np., moge odpalać na 2 sposoby
+    main(sys.argv[1])
+    # read_file(sys.argv[1])  # przkeazywanie parametru po nazwie pliku
